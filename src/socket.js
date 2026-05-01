fb459ceaf680ae6830b7e78eff9e6ff3e2e7aa02
Atharv3904 <atharvkulkarni3904@gmail.com>
2026-05-01 18:58:20 +0530

    all frontend connection Done

--- src/socket.js
+++ src/socket.js
@@ -5,7 +5,10 @@ const rooms = new Map();
 function setupSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+      origin: [
+        "http://localhost:5173",
+        "https://new-draw-frontend.vercel.app/",
+      ],
       methods: ["GET", "POST"],
     },
   });
